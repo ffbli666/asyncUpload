36d7efb546f8ebad87c9183e1f358756cacb4ddb
MagicPeter <ffbli@MagicPeterteki-MacBook-Air.local>
2014-07-21 19:11:35 +0800

    refactory more easy used

--- js/common.js
+++ js/common.js
@@ -8,19 +8,12 @@
 
 (function($) {
     $.asyncSend = function (options) {
+        var self = this;
         var dfd = $.Deferred();     
         var send_xhr = {};
-        var xhr = function() {
-            var xhr = new window.XMLHttpRequest();
-            xhr.upload.addEventListener('progress', function(evt) {             
-                var percentComplete = (evt.loaded / evt.total) * 100;
-                dfd.notify(percentComplete);
-            }, false);
-            return xhr;
-        };
+        var file;
 
         var options = $.extend({
-            xhr:xhr,
             type: 'POST',
             dataType: 'json',
             cache: false,
@@ -29,11 +22,21 @@
             url: undefined,
             data: undefined
         }, options);
-        
+
+        //console.log(options);
         if (options.url == undefined || options.data == undefined) {
             console.log('need url or data');
             return false;
         }
+
+        options.xhr = function() {
+            var xhr = new window.XMLHttpRequest();
+            xhr.upload.addEventListener('progress', function(evt) {             
+                var percentComplete = (evt.loaded / evt.total) * 100;
+                dfd.notify(percentComplete);
+            }, false);
+            return xhr;
+        };
 
         var send = function() {             
             send_xhr = $.ajax(options);
@@ -66,53 +69,7 @@
         };
     };  
 
-    $.fn.extend({ 
-        progressObject: function() {
-            var self = this;
-            var item = self.find('[var=item_template]')
-            self.empty();
-            var fileProgress = function (file) {
-                var file = file;
-                var new_item = item.clone();                   
-                new_item.find('[var=filename]').html(file.name);
-                self.append(new_item);
-                var progress = function (percentComplete) {                               
-                    new_item.find('[var=progress-bar]').css('width', percentComplete + '%');
-                    new_item.find('[var=progress-value]').html(parseInt(percentComplete) + '%');
-                };
-                var done = function(response) {
-                    new_item.find('[var=cancel]').off('click').html('Completed');
-                };
-                var fail = function(response, status, error) {  
-
-                };
-                var always = function(always) {
-
-                };
-                var setCancel = function (asyncSend) {                  
-                    new_item.find('[var=cancel]').on('click', function() {
-                        asyncSend.abort();
-                        new_item.find('[var=cancel]').html("Canceled");
-                        new_item.find('[var=progress-bar]').css('width', '0%');
-                        new_item.find('[var=progress-value]').html('');
-                    });
-                };
-                return {
-                    file : file,
-                    progress : progress,
-                    setCancel : setCancel,
-                    done : done,
-                    fail : progress,
-                    always : always
-                };
-            }
-
-
-            return {    
-                fileProgress : fileProgress,                            
-            };
-        },  
-
+    $.fn.extend({     
         asyncUpload: function(options) {
             var self = this;            
             var options = $.extend({
@@ -120,11 +77,11 @@
                 name: self.attr('name'),
                 multiple: (self.attr('multiple') == 'multiple') ? true : false,
                 preCheck: function (files) { return true; },
-                //preSend: function (file) {},
-                progress: function (percentComplete) { console.log('[upload] default progress. ' + percentComplete + '%'); },
-                done: function (response){ console.log('[upload] Need implement done'); },
-                fail: function (response, status, error){ console.log('[upload] Need implement fail. status: ' + status + ', error: ' + error); },
-                abort: function(asyncSend) { console.log('[upload abort]') },
+                preSend: function (file) { return true; },
+                progress: function (file, percentComplete) {},
+                done: function (response) {},
+                fail: function (response, status, error) {},
+                abort: function(asyncSend) {},
                 always: function () {},
                 progressObject: undefined
             }, options);
@@ -156,47 +113,27 @@
                 }
             }
 
-            if (options.progressObject != undefined)
-            {
-                //options.preCheck = options.progressObject.preCheck;
-                //options.progress = options.progressObject.progress;
-                //options.preSend = options.progressObject.preSend;
-            }
-
             self_input.on('change', {}, function(evt) { 
                 evt.preventDefault();
 
-                if (!options.preCheck.call(self, self_input[0].files)) {
-                    console.log("[upload] preCheck error");
+                if (!options.preCheck.call(self, self_input[0].files)) {                        
                     return; 
                 }                               
-                var up;
+
                 for(var i=0; i<self_input[0].files.length; i++) {
-                    //pre send
-                    //options.preSend.call(self, self_input[0].files[i]);
-                    var fileProgress = options.progressObject.fileProgress(self_input[0].files[i]);
-                    console.log(fileProgress);
                     //create fomedata
                     var formdata = new FormData();                  
                     formdata.append(options.name, self_input[0].files[i]);
                     //init asyncSend
-                    var asyncSend = $.asyncSend({
+                    var asyncSend = new $.asyncSend({
                         url: options.url,
-                        data: formdata
+                        data: formdata,
+                        file: self_input[0].files[i],
                     });
-                    //register asyncSend event
-                    asyncSend.getDeferred()
-                    .progress(fileProgress.progress)
-                    .done(fileProgress.done)
-                    .fail(fileProgress.fail)
-                    .always(fileProgress.always)
 
-
-                    //triggle upload                    
-                    asyncSend.send();
-                    //options.abort.call(self, asyncSend);
-                    //options.progressObject.setCancel.call(self, asyncSend);
-                    fileProgress.setCancel.call(self, asyncSend);
+                    if (!options.preSend.call(self, self_input[0].files[i], asyncSend)) {
+                        continue;
+                    } 
                 }
                 self_input[0].value = '';
             });
