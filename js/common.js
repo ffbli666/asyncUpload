ffefdff37641ab7bea6326d7d6b6abde9fb01aed
Zong-Ying Lyu <ffbli666@gmail.com>
2014-07-21 20:59:49 +0800

    1. refactory 2. add allDone event for wait all the asyncSend done 3. add somdFail event for wait all the asyncSend done but some failed 4. improve demo html

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 /**
  * 
  * 
- *
+ *  Zong-Ying Lyu : ffbli666@gmail.com
  */
 
 
@@ -9,9 +9,8 @@
 (function($) {
     $.asyncSend = function (options) {
         var self = this;
-        var dfd = $.Deferred();     
-        var send_xhr = {};
-        var file;
+        var dfd = $.Deferred();
+        var send_xhr = {};        
 
         var options = $.extend({
             type: 'POST',
@@ -23,7 +22,6 @@
             data: undefined
         }, options);
 
-        //console.log(options);
         if (options.url == undefined || options.data == undefined) {
             console.log('need url or data');
             return false;
@@ -31,16 +29,16 @@
 
         options.xhr = function() {
             var xhr = new window.XMLHttpRequest();
-            xhr.upload.addEventListener('progress', function(evt) {             
+            xhr.upload.addEventListener('progress', function(evt) {
                 var percentComplete = (evt.loaded / evt.total) * 100;
                 dfd.notify(percentComplete);
             }, false);
             return xhr;
         };
 
-        var send = function() {             
+        var send = function() {
             send_xhr = $.ajax(options);
-            send_xhr.pipe(function(response) {              
+            send_xhr.pipe(function(response) {
                 return response;
             })
             .fail(function(xhr, status, error) {
@@ -71,19 +69,17 @@
 
     $.fn.extend({     
         asyncUpload: function(options) {
-            var self = this;            
-            var options = $.extend({
+            var self = this;
+            var self_input = undefined;
+            var options = $.extend( {
+                dataType: 'json',
                 url: undefined,
                 name: self.attr('name'),
                 multiple: (self.attr('multiple') == 'multiple') ? true : false,
                 preCheck: function (files) { return true; },
-                preSend: function (file) { return true; },
-                progress: function (file, percentComplete) {},
-                done: function (response) {},
-                fail: function (response, status, error) {},
-                abort: function(asyncSend) {},
-                always: function () {},
-                progressObject: undefined
+                preSend: function (file, asyncSend) { return true;},
+                allDone: function () {},
+                someFail: function () {}
             }, options);
             
             
@@ -92,15 +88,14 @@
                 return false;
             }
             
-            var self_input = undefined;         
-
+   
             if (!self.is('input')) {
-                var new_input = document.createElement('input')                             
-                new_input.setAttribute('type','file');                  
+                var new_input = document.createElement('input')
+                new_input.setAttribute('type','file');
                 self_input = $(new_input);
-                self.on('click', function(evt){
+                self.on('click', function(evt) {
                     self_input.trigger('click');
-                })
+                });
             }
             else {
                 self_input = self;
@@ -114,18 +109,21 @@
             }
 
             self_input.on('change', {}, function(evt) { 
+                var asyncSends = [];
                 evt.preventDefault();
 
-                if (!options.preCheck.call(self, self_input[0].files)) {                        
+                if (!options.preCheck.call(self, self_input[0].files)) {
                     return; 
                 }                               
 
                 for(var i=0; i<self_input[0].files.length; i++) {
                     //create fomedata
-                    var formdata = new FormData();                  
+                    var formdata = new FormData();              
                     formdata.append(options.name, self_input[0].files[i]);
+                    
                     //init asyncSend
                     var asyncSend = new $.asyncSend({
+                        dataType: options.dataType,
                         url: options.url,
                         data: formdata,
                         file: self_input[0].files[i],
@@ -133,8 +131,11 @@
 
                     if (!options.preSend.call(self, self_input[0].files[i], asyncSend)) {
                         continue;
-                    } 
+                    }                    
+                    asyncSends.push(asyncSend.getDeferred());
                 }
+                
+                $.when.apply($, asyncSends).then(options.allDone, options.someFail);
                 self_input[0].value = '';
             });
             return this;
